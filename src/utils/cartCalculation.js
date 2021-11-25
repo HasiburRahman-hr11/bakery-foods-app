48c12ee2fbdf5f81d767ca30ced0d7392d08ee33
hrhasib <mailtohrhasib@gmail.com>
2021-11-25 13:15:04 +0600

    css polishing

--- src/utils/cartCalculation.js
+++ src/utils/cartCalculation.js
@@ -1,7 +1,7 @@
 
 const calculateCartAmount = (cartFoods) => {
     const subtotal = cartFoods.reduce((p, c) => p + c.price * c.quantity, 0);
-    const shipping = 10;
+    const shipping = 5;
     const tax = (subtotal + shipping) * 10 / 100;
     const totalPrice = subtotal + shipping + tax;
     return {
